3d0f25a341fc4dfa2a9f41a4a66bf9e0f421280e
Fedorova Galina <fga@mail.ru>
2022-11-06 21:34:39 +0300

    fix 061122 16

--- SF_DST/Python-8/game2.py
+++ SF_DST/Python-8/game2.py
@@ -4,7 +4,7 @@
 import numpy as np
 
 def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
+    """Рандомно угадываем число 
 
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
